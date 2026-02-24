16c09657d9a56ce9bf8d6a94f57ac94fc73ae54c
MandaStrong01 <woolleya129@gmail.com>
2026-02-24 05:25:20 -0500

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,18 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  server: {
+    host: true,
+    port: 5173
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
   build: {
     manifest: true,
+    outDir: 'dist',
+    sourcemap: false,
     rollupOptions: {
       output: {
         manualChunks: {
